9a6d8e91adcd302a4d29466ed352ddbfebb14d7a
Pawel <pawel80@gmail.com>
2024-01-16 21:51:50 +0100

    Azure TF v5

--- tf_azure/infra.tf
+++ tf_azure/infra.tf
@@ -1,4 +1,4 @@
-resource "azure_resource_group" "rg_func" {
+resource "azurerm_resource_group" "rg_func" {
     name = "${var.resource_group_name}"
     location = "${var.resource_group_location}"
 }
