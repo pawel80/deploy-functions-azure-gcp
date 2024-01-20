8389896b347b8ecffdde1725e378114b20d9e224
Pawel <pawel80@gmail.com>
2024-01-20 16:02:49 +0100

    Azure tf remote state v1

--- tf_azure/providers.tf
+++ tf_azure/providers.tf
@@ -5,6 +5,12 @@ terraform {
       # version = "~> 3.87.0"
     }
   }
+  cloud {
+    organization = "tf-pawel-org"
+    workspaces {
+      name = "tf-public-cloud"
+    }
+  }
   # required_version = ">= 1.1.0"
 }
 
