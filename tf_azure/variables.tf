e96c253780614571e851ee3e053d4ac8dac7408b
Pawel <pawel80@gmail.com>
2024-01-21 14:54:24 +0100

    Fixed README.md

--- tf_azure/variables.tf
+++ tf_azure/variables.tf
@@ -1,20 +1,5 @@
 # TF destroy will try to read var.resource, ..., var.api_username, var.api_password
 # so we need to provide some default values
-  
-variable "resource_group_name" {
-  description = "Resource group name"
-  type = string
-  default = "RG-TF-TEST-FUNC"
-}
-
-variable "resource_group_location" {
-  description = "Resource group location"
-  type = string
-  # default = "westeurope"
-  # default = "italynorth"
-  default = "polandcentral"
-}
-
 variable "ARM_SUBSCRIPTION_ID" {
   description = "Azure subscription id"
   type = string
@@ -37,4 +22,18 @@ variable "ARM_CLIENT_SECRET" {
   description = "Azure service principal secret"
   type = string
   default = ""  
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type = string
+  default = "RG-TF-TEST-FUNC"
+}
+
+variable "resource_group_location" {
+  description = "Resource group location"
+  type = string
+  # default = "westeurope"
+  # default = "italynorth"
+  default = "polandcentral"
 }
