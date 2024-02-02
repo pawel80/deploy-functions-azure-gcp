e40570e99849fe3c8a86445bb131909494d951ca
Pawel <pawel80@gmail.com>
2024-02-02 21:35:57 +0100

    GCP and Azure workflows v2

--- tf_azure/variables.tf
+++ tf_azure/variables.tf
@@ -1,28 +1,26 @@
-# TF destroy will try to read var.resource, ..., var.api_username, var.api_password
-# so we need to provide some default values
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "Azure subscription id"
-  type = string
-  default = ""
-}
+# variable "ARM_SUBSCRIPTION_ID" {
+#   description = "Azure subscription id"
+#   type = string
+#   default = ""
+# }
 
-variable "ARM_TENANT_ID" {
-  description = "Azure tenant id"
-  type = string
-  default = "" 
-}
+# variable "ARM_TENANT_ID" {
+#   description = "Azure tenant id"
+#   type = string
+#   default = "" 
+# }
 
-variable "ARM_CLIENT_ID" {
-  description = "Azure service principal id"
-  type = string
-  default = ""
-}
+# variable "ARM_CLIENT_ID" {
+#   description = "Azure service principal id"
+#   type = string
+#   default = ""
+# }
 
-variable "ARM_CLIENT_SECRET" {
-  description = "Azure service principal secret"
-  type = string
-  default = ""  
-}
+# variable "ARM_CLIENT_SECRET" {
+#   description = "Azure service principal secret"
+#   type = string
+#   default = ""  
+# }
 
 variable "resource_group_name" {
   description = "Resource group name"
