d949df98af71305e10d7ca24cb1bad9c86e0c3df
Pawel <pawel80@gmail.com>
2024-02-03 20:39:01 +0100

    GCP-Terraform workflow v1

--- tf_gcp/providers.tf
+++ tf_gcp/providers.tf
@@ -18,5 +18,5 @@ provider "google" {
   credentials = "${var.GCP_CREDENTIALS}"
   project     = "${var.GCP_PROJECT_ID}"
   # region      = var.GCP_REGION
-  # zone        = var.zone
+  # zone        = var.GCP_ZONE
 }
--- tf_gcp/variables.tf
+++ tf_gcp/variables.tf
@@ -10,16 +10,7 @@ variable "GCP_PROJECT_ID" {
   default = ""
 }
 
-# variable "resource_group_name" {
-#   description = "Resource group name"
-#   type = string
-#   default = "RG-TF-TEST-FUNC"
-# }
-
-# variable "resource_group_location" {
-#   description = "Resource group location"
-#   type = string
-#   # default = "westeurope"
-#   # default = "italynorth"
-#   default = "polandcentral"
-# }
+# In GCP there are no RG, resources are places in Projects
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
