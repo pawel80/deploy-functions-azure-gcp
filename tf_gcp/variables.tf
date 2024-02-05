9104387bfbbdc536d155c6dcabb6b30de06dab06
Pawel <pawel80@gmail.com>
2024-02-05 22:05:45 +0100

    GCP-Terraform workflow v22

--- tf_gcp/variables.tf
+++ tf_gcp/variables.tf
@@ -1,11 +1,11 @@
-variable "GOOGLE_CREDENTIALS" {
-  description = "GCP credentials"
-  type    = string
-  default = ""
-}
+# variable "GOOGLE_CREDENTIALS" {
+#   description = "GCP credentials"
+#   type    = string
+#   default = ""
+# }
 
-variable "GOOGLE_PROJECT_ID" {
-  description = "GCP project id"
-  type    = string
-  default = ""
-}
+# variable "GOOGLE_PROJECT_ID" {
+#   description = "GCP project id"
+#   type    = string
+#   default = ""
+# }
