a77f9293212a517a8c1edd174c88ed08ee294b30
Pawel <pawel80@gmail.com>
2024-02-08 19:59:27 +0100

    workflows conditions v6

--- tf_azure/providers.tf
+++ tf_azure/providers.tf
@@ -25,8 +25,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  tenant_id         = "${var.ARM_TENANT_ID}"
-  subscription_id   = "${var.ARM_SUBSCRIPTION_ID}"
-  client_id         = "${var.ARM_CLIENT_ID}"
-  client_secret     = "${var.ARM_CLIENT_SECRET}"
+  # tenant_id         = "${var.ARM_TENANT_ID}"
+  # subscription_id   = "${var.ARM_SUBSCRIPTION_ID}"
+  # client_id         = "${var.ARM_CLIENT_ID}"
+  # client_secret     = "${var.ARM_CLIENT_SECRET}"
 }
