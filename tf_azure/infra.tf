7763794810af33a51e629c4f262a47583546bba8
Pawel <pawel80@gmail.com>
2024-02-10 17:09:08 +0100

    Azure_Function_v1

--- tf_azure/infra.tf
+++ tf_azure/infra.tf
@@ -2,3 +2,31 @@ resource "azurerm_resource_group" "rg_func" {
     name = "${var.resource_group_name}"
     location = "${var.resource_group_location}"
 }
+
+# resource "azurerm_storage_account" "example" {
+#   name                     = "linuxfunctionappsa"
+#   resource_group_name      = azurerm_resource_group.example.name
+#   location                 = azurerm_resource_group.example.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+
+# resource "azurerm_service_plan" "example" {
+#   name                = "example-app-service-plan"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   os_type             = "Linux"
+#   sku_name            = "Y1"
+# }
+
+# resource "azurerm_linux_function_app" "example" {
+#   name                = "example-linux-function-app"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+
+#   storage_account_name       = azurerm_storage_account.example.name
+#   storage_account_access_key = azurerm_storage_account.example.primary_access_key
+#   service_plan_id            = azurerm_service_plan.example.id
+
+#   site_config {}
+# }
