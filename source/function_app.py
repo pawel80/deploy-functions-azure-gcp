c852725e97061d8343f8f476ef241b71cd687a2c
Pawel <pawel80@gmail.com>
2024-03-07 21:41:32 +0100

    Azure_Function_deploy_v1

---
+++ source/function_app.py
@@ -0,0 +1,39 @@
+import azure.functions as func
+# import datetime
+# import json
+import logging
+
+app = func.FunctionApp()
+
+@app.function_name(name="HttpTrigger1")
+@app.route(route="py_https", auth_level=func.AuthLevel.ANONYMOUS)
+def py_https(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    name = req.params.get("name")
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get("name")
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+
+@app.function_name(name="HttpTrigger2")
+@app.route(route="py_https2", auth_level=func.AuthLevel.ANONYMOUS)
+def py_https2(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function (two) processed a request.")
+
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully.",
+        status_code=200
+        )
